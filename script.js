93ddf9c4b8f939ce087c0afa329d103b9545c412
NamanJain14101999 <nj648515@gmail.com>
2021-09-28 18:50:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+fetch('https://icanhazdadjoke.com/slack')
+.then(data => data.json())
+.then(jokeData => {
+    const joketext = jokeData.attachments[0].text;
+    const jokeelement = document.getElementById("#jokeElement")
+    jokeElement.innerText = joketext;
+})
